e5a5e21d178535a1bfd08bc5ba37bd72d5cbacda
Lapa Daluki <amoslegkie@outlook.com>
2022-09-04 01:50:52 +0300

    Wrote a test function. Doesn't work correct, I'm tired and want to sleep

---
+++ test_items.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import unittest
+import time
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_exist_button(browser):
+    browser.get(link)
+    #language = browser.find_element(By.TAG_NAME, "html").get_attribute("lang")
+    button = browser.find_element(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
+    button.click()
+    #time.sleep(5)
+    text1 = browser.find_element(By.CLASS_NAME, "alertinner").text
+    assert len(text1) != 0, "Somethong gone wrong. Try to add item again"
+    browser.quit()
