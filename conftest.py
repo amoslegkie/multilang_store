06a126597b3bd2dedaaea3c317ca15c6494bd194
Lapa Daluki <amoslegkie@outlook.com>
2022-09-04 11:33:40 +0300

    Finaly this file works correct

--- conftest.py
+++ conftest.py
@@ -7,31 +7,16 @@ def pytest_addoption(parser):
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
                      help="Choose language: es or fr")
-'''   parser.addoption('--pause', action='store', default=0, help="Включение паузы после загрузки")'''
-
-'''@pytest.fixture(scope="function")
-def pause_time(request):
-    pause_mode = request.config.getoption("pause")
-    return pause_mode'''
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        #browser = webdriver.Chrome(options=options)        
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        #browser = webdriver.Firefox(firefox_profile=fp)
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    #browser = None
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)        
+    print("\nstart chrome browser for test..")
     yield browser
     print("\nquit browser..")
     browser.quit()
